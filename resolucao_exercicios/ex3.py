b7ce6e5084c753450e5d1b78dee15887eb29dbe4
Lucas <lucasbarbosa051298@gmail.com>
2023-05-27 16:39:14 -0300

    alterando os nomes

---
+++ resolucao_exercicios/ex3.py
@@ -0,0 +1,11 @@
+#Questão 3
+#Para a construção de programas flexíveis e mais complexos, é necessário que haja a realização de comparações. Isso é possível com o uso de operadores lógicos de comparação (em python: ==, !=, <, <=, >, >=), como mostra o código:
+
+x = 10
+y = 4.2
+
+num = float(input("Digite um número a seguir: "))
+
+print(num > x*y, num <= x + y, num*y != x*y)
+
+#R: False True True
