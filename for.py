bbab975f0d9855584266da9c54992966f5832fc3
Lucas <lucasbarbosa051298@gmail.com>
2023-05-27 18:40:08 -0300

    add for and while

---
+++ for.py
@@ -0,0 +1,30 @@
+#OBS: REMOVER AS ASPAS :)
+
+'''for variavel in range(5):
+    print(variavel)'''
+
+'''
+for variavel in range(1,10):
+    print(variavel)
+
+'''
+
+'''
+#1, 4, 7, 10
+for variavel in range(1,12,3):
+    print(variavel)
+
+'''
+
+'''
+#Calculando a media
+
+soma = 0
+
+for i in range(1,4):
+    nota = float(input(f"Informe sua nota{i}:")) # conhecimento novo: usar "f" e "{}" para colocar uma variavel dentro da string. Att Python3.
+    soma = soma + nota
+
+print(soma/3)
+    
+'''
---
+++ while.py
@@ -0,0 +1,29 @@
+#EX01 Estrtutura simples
+numero_sorteado = 15
+
+numero_escolhido = int(input("Informe um numero entre 1 e 20:"))
+
+i = 0
+while i <= 3:
+
+    numero_escolhido != numero_sorteado
+    print("Você errou o numero, tente novamente...")
+
+    numero_escolhido = int(input("Informe um numero entre 1 e 20: "))
+
+print("Parabéns, você acertou!")
+
+#EX02 Estrutura com contador 
+
+contador = 0
+
+while contador <5:
+    print (contador)
+
+    contador = contador + 1
+
+
+
+
+
+
